c04c2d734a67782d071c3f2cfdb71f3c844d20c2
NIXBLACK11 <Siddharthsinghrana11@gmail.com>
2023-08-09 19:34:26 +0530

    gg

--- nltk_utils.py
+++ nltk_utils.py
@@ -2,42 +2,44 @@ import numpy as np
 import nltk
 # nltk.download('punkt')
 from nltk.stem.porter import PorterStemmer
+
 stemmer = PorterStemmer()
 
-def tokenize(sentence):
+def tokenize_text(input_text):
     """
-    split sentence into array of words/tokens
-    a token can be a word or punctuation character, or number
+    Split input_text into a list of words/tokens.
+    A token can be a word, punctuation character, or number.
     """
-    return nltk.word_tokenize(sentence)
+    return nltk.word_tokenize(input_text)
 
-
-def stem(word):
+def stem_word(word):
     """
-    stemming = find the root form of the word
-    examples:
+    Apply stemming to find the root form of a word.
+    Example:
     words = ["organize", "organizes", "organizing"]
-    words = [stem(w) for w in words]
+    stemmed_words = [stem_word(w) for w in words]
     -> ["organ", "organ", "organ"]
     """
     return stemmer.stem(word.lower())
 
-
-def bag_of_words(tokenized_sentence, words):
+def create_bag_of_words(tokenized_sentence, vocabulary):
     """
-    return bag of words array:
-    1 for each known word that exists in the sentence, 0 otherwise
-    example:
-    sentence = ["hello", "how", "are", "you"]
-    words = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
-    bog   = [  0 ,    1 ,    0 ,   1 ,    0 ,    0 ,      0]
+    Generate a bag of words array:
+    1 for each word in the vocabulary that exists in the tokenized_sentence, 0 otherwise.
+    Example:
+    tokenized_sentence = ["hello", "how", "are", "you"]
+    vocabulary = ["hi", "hello", "I", "you", "bye", "thank", "cool"]
+    bag_of_words = [  0 ,    1 ,    0 ,   1 ,    0 ,    0 ,      0]
     """
-    # stem each word
-    sentence_words = [stem(word) for word in tokenized_sentence]
-    # initialize bag with 0 for each word
-    bag = np.zeros(len(words), dtype=np.float32)
-    for idx, w in enumerate(words):
-        if w in sentence_words: 
+    # Apply stemming to each word in the tokenized sentence
+    stemmed_words = [stem_word(word) for word in tokenized_sentence]
+    
+    # Initialize the bag with zeros for each word in the vocabulary
+    bag = np.zeros(len(vocabulary), dtype=np.float32)
+    
+    # Set corresponding entries to 1 for words present in the tokenized sentence
+    for idx, word in enumerate(vocabulary):
+        if word in stemmed_words:
             bag[idx] = 1
-
+    
     return bag
--- train.py
+++ train.py
@@ -10,50 +10,40 @@ from nltk_utils import bag_of_words, tokenize, stem
 from model import NeuralNet
 
 with open('intents.json', 'r') as f:
-    intents = json.load(f)
+    intents_data = json.load(f)
 
 all_words = []
 tags = []
-xy = []
-# loop through each sentence in our intents patterns
-for intent in intents['intents']:
+xy_pairs = []
+
+for intent in intents_data['intents']:
     tag = intent['tag']
-    # add to tag list
     tags.append(tag)
     for pattern in intent['patterns']:
-        # tokenize each word in the sentence
-        w = tokenize(pattern)
-        # add to our words list
-        all_words.extend(w)
-        # add to xy pair
-        xy.append((w, tag))
+        words = tokenize(pattern)
+        all_words.extend(words)
+        xy_pairs.append((words, tag))
 
-# stem and lower each word
 ignore_words = ['?', '.', '!']
-all_words = [stem(w) for w in all_words if w not in ignore_words]
-# remove duplicates and sort
+all_words = [stem(word) for word in all_words if word not in ignore_words]
 all_words = sorted(set(all_words))
 tags = sorted(set(tags))
 
-print(len(xy), "patterns")
+print(len(xy_pairs), "pattern pairs")
 print(len(tags), "tags:", tags)
 print(len(all_words), "unique stemmed words:", all_words)
 
-# create training data
 X_train = []
 y_train = []
-for (pattern_sentence, tag) in xy:
-    # X: bag of words for each pattern_sentence
-    bag = bag_of_words(pattern_sentence, all_words)
+for (pattern_words, tag) in xy_pairs:
+    bag = bag_of_words(pattern_words, all_words)
     X_train.append(bag)
-    # y: PyTorch CrossEntropyLoss needs only class labels, not one-hot
     label = tags.index(tag)
     y_train.append(label)
 
 X_train = np.array(X_train)
 y_train = np.array(y_train)
 
-# Hyper-parameters 
 num_epochs = 1000
 batch_size = 8
 learning_rate = 0.001
@@ -63,65 +53,51 @@ output_size = len(tags)
 print(input_size, output_size)
 
 class ChatDataset(Dataset):
-
     def __init__(self):
-        self.n_samples = len(X_train)
+        self.num_samples = len(X_train)
         self.x_data = X_train
         self.y_data = y_train
 
-    # support indexing such that dataset[i] can be used to get i-th sample
     def __getitem__(self, index):
         return self.x_data[index], self.y_data[index]
 
-    # we can call len(dataset) to return the size
     def __len__(self):
-        return self.n_samples
+        return self.num_samples
 
 dataset = ChatDataset()
-train_loader = DataLoader(dataset=dataset,
-                          batch_size=batch_size,
-                          shuffle=True,
-                          num_workers=0)
+train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True, num_workers=0)
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 model = NeuralNet(input_size, hidden_size, output_size).to(device)
 
-# Loss and optimizer
 criterion = nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
-# Train the model
 for epoch in range(num_epochs):
-    # To iterate each batch of the dataset
     for (words, labels) in train_loader:
         words = words.to(device)
         labels = labels.to(dtype=torch.long).to(device)
         
-        # Forward pass
         outputs = model(words)
-        # if y would be one-hot, we must apply
-        # labels = torch.max(labels, 1)[1]
         loss = criterion(outputs, labels)
         
-        # Backward and optimize The gradients are then zeroed, the backward pass is performed, and the optimizer's step function is called to update the model's parameters.
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-    # Every 100 epochs, the current loss is printed.
+    
     if (epoch+1) % 100 == 0:
-        print (f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
-
+        print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
 
 print(f'final loss: {loss.item():.4f}')
 
 data = {
-"model_state": model.state_dict(),
-"input_size": input_size,
-"hidden_size": hidden_size,
-"output_size": output_size,
-"all_words": all_words,
-"tags": tags
+    "model_state": model.state_dict(),
+    "input_size": input_size,
+    "hidden_size": hidden_size,
+    "output_size": output_size,
+    "all_words": all_words,
+    "tags": tags
 }
 
 FILE = "data.pth"
