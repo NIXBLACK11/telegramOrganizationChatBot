698bb21f88c98f96ddf76fffeb68f2d2d8d84fa1
NIXBLACK11 <Siddharthsinghrana11@gmail.com>
2024-02-08 18:23:25 +0530

    train code

--- v2/chatBot.py
+++ v2/chatBot.py
@@ -1,19 +1,56 @@
-from laser_encoders import LaserEncoderPipeline
+import numpy as np
 import torch
+import torch.nn as nn
+import torch.optim as optim
+import random
 import json
+import pickle
+from laser_encoders import LaserEncoderPipeline
 
-model = torch.load('./model.pth', map_location=torch.device('cpu'))
 encoder = LaserEncoderPipeline(lang="eng_Latn")
+
+with open('sizes.pkl', 'rb') as file:
+    sizes = pickle.load(file)
+
+
+with open('lables.pkl', 'rb') as file:
+    labels = pickle.load(file)
 
 with open('intents.json') as file:
     data = json.load(file)
+
+input_size = sizes[0]
+output_size = sizes[1]
+hidden_size = sizes[2]
+
+class ChatbotModel(nn.Module):
+  def __init__(self, input_size, hidden_size, output_size):
+    super(ChatbotModel, self).__init__()
+    self.l1 = nn.Linear(input_size, hidden_size)
+    self.l2 = nn.Linear(hidden_size, hidden_size)
+    self.l3 = nn.Linear(hidden_size, output_size)
+    self.relu = nn.ReLU()
+
+  def forward(self, x):
+    out = self.l1(x)
+    out = self.relu(out)
+    out = self.l2(out)
+    out = self.relu(out)
+    out = self.l3(out)
+    return out
+
+state_dict = torch.load('model.pth')
+model = ChatbotModel(input_size, hidden_size, output_size)
+model.load_state_dict(state_dict)
+model.eval()
+
 
 def chat(user_input):
     user_input = user_input.lower()
     input_bag = encoder.encode_sentences([user_input])[0]
 
-    input_bag = torch.tensor(input_bag, dtype=torch.float32).to(device)
-    input_bag = input_bag.unsqueeze(0)
+    input_bag = torch.tensor(input_bag, dtype=torch.float32)
+    input_bag = input_bag.unsqueeze(0)  # Add a batch dimension
 
     results = model(input_bag)
     results_index = torch.argmax(results).item()
@@ -22,3 +59,14 @@ def chat(user_input):
     for intent in data['intents']:
         if intent['tag'] == tag:
             return random.choice(intent['responses'])
+
+
+
+# sentences = ["Hi", "who made you", "what are the bank timings", "what is my account balance", "where is the bank located",
+#               "नमस्ते", "आपको किसने बन", "बैंक का समय क्या", "मेरे खाते का शेष राशि क्", "बैंक कहाँ स्थित है?",
+#               "Hola", "¿Quién te creó?", "¿Cuáles son los horarios del banco?", "¿Cuál es el saldo de mi cuenta?", "¿Dónde está ubicado el banco?",
+#               "what documents are required to open an account", "खाता खोलने के लिए कौन-कौन से दस्तावेज़ आ", "¿Qué documentos se requieren para abrir una cuenta?"]
+
+# for sentence in sentences:
+#   print(f"You: {sentence}")
+#   print(f"Bot: {process_input(sentence)}")
